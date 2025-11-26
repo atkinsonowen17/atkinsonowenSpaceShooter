1d74c62f1700cf3e90f92203df3a11696598db05
Owen Atkinson <s4013303@student.rmit.edu.au>
2025-11-26 19:51:05 +1100

    Added shooting

---
+++ atkinsonowen17SpaceShooter/Assets/Scripts/FireballController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FireballController : MonoBehaviour
+{
+    // public float speed = 5f;
+    // public float lifetime = 5f;
+
+    // void Start()
+    // {
+    //     Destroy(gameObject, lifetime);
+    // }
+
+    // void Update()
+    // {
+    //     transform.position += Vector3.right * speed * Time.deltaTime;
+    // }
+}
--- atkinsonowen17SpaceShooter/Assets/Scripts/ShipControler.cs
+++ atkinsonowen17SpaceShooter/Assets/Scripts/ShipControler.cs
@@ -7,6 +7,9 @@ public class ShipController : MonoBehaviour
     public float yLimit = 3.5f;
     public float fireDelay = 0.3f;
     public float lastFire;
+    public GameObject fireballPrefab;
+    public GameObject fireballPrefab2;
+    public float bulletSpeed = 10f;
 
     void Start()
     {
@@ -34,5 +37,14 @@ public class ShipController : MonoBehaviour
     public void Shoot()
     {
         Debug.Log("shoot");
+
+        Vector3 spawnPos = transform.position + Vector3.right * 1.3F;
+
+        GameObject fireball = Instantiate(fireballPrefab, spawnPos, transform.rotation);
+
+        Destroy(fireball, 1f);
+
+        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = -transform.up * bulletSpeed;
     }
 }
